5372248dc17729428e5b19daffb4c7fea0ae5ee5
Nick <kosmynin84@outlook.com>
2022-04-06 13:20:19 +0300

    simple frame

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,20 @@ let slideIndex = 1; //переменная кот. в будущем будет 
 
 let offset = 0; //отступ
 
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
 slidesField.style.width = 100 * slides.length + '%';
 slidesField.style.display = 'flex';
 slidesField.style.transition = '0.5s all';
@@ -29,7 +43,7 @@ slides.forEach(slide => {
 const dots = [];
 
 
-const dotsLi = document.querySelectorAll('li');
+const dotsLi = document.querySelectorAll('.slider__indicators-dot');
 
 dotsLi.forEach((dot, i) => {
     dot.setAttribute('data-slice-to', i + 1);
@@ -37,6 +51,7 @@ dotsLi.forEach((dot, i) => {
         dot.style.background = 'black';
         dot.style.width = '20px';
         dot.style.height = '20px';
+
     }
     dots.push(dot);
 });
@@ -45,6 +60,8 @@ dotsLi.forEach((dot, i) => {
 
 next.addEventListener('click', (e) => {
     e.preventDefault();
+
+    plusSlides(1);
 
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
         // ширина слайда умноженное на кол слайдов
@@ -56,11 +73,14 @@ next.addEventListener('click', (e) => {
     slidesField.style.transform = `translateX(-${offset}px)`;
 
     dots.forEach(dot => dot.style.opacity = '.5');
+
     dots[slideIndex - 1].style.opacity = 1;
 });
 
 prev.addEventListener('click', (e) => {
     e.preventDefault();
+
+    plusSlides(-1);
 
     if (offset == 0) {
 
@@ -74,38 +94,39 @@ prev.addEventListener('click', (e) => {
 });
 
 
+// slider2
 
+const frame = document.querySelectorAll('.comment__slide'),
+    early = document.querySelector('.comment__arrow-left'),
+    later = document.querySelector('.comment__arrow-right'),
+    frameField = document.querySelector('.comment__slider-inner');
 
+let frameIndex = 1; // переменная в будущем будет меняться
 
+showFrame(frameIndex);
 
+function showFrame(n) {
+    if (n > frame.length) {
+        frameIndex = 1;
+    }
 
+    if (n < 1) {
+        frameIndex = frame.length;
+    }
 
+    frame.forEach(item => item.style.display = 'none');
 
+    frame[frameIndex - 1].style.display = 'block';
+}
 
+function plusFrame(n) {
+    showFrame(frameIndex += n);
+}
 
+early.addEventListener('click', () =>{
+    plusFrame(-1);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-// function showSlides(n) {
-//     if (n > slides.length){ //если ушли в правую границу, то перемещаемся в нач.
-//         slideIndex = 1;
-//     }
-
-//     if(n < 1) {
-//         slideIndex = slides.length;  //если ушли в левую границу, то перемещаемся 
-//         //в конец
-
-//     }  
-
-
-
-// }
+later.addEventListener('click', ()=>{
+    plusFrame(1);
+});
